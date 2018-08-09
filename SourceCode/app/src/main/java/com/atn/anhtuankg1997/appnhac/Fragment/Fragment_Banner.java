60fc05a05124f668272b19080b3f6594bcf35144
ryananh1997 <42193663+ryananh1997@users.noreply.github.com>
2018-08-09 08:23:34 +0700

    Lession9
    Doc du lieu api cua quang cao

--- SourceCode/app/src/main/java/com/atn/anhtuankg1997/appnhac/Fragment/Fragment_Banner.java
+++ SourceCode/app/src/main/java/com/atn/anhtuankg1997/appnhac/Fragment/Fragment_Banner.java
@@ -1,16 +1,25 @@
 package com.atn.anhtuankg1997.appnhac.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.atn.anhtuankg1997.appnhac.Model.Quangcao;
 import com.atn.anhtuankg1997.appnhac.R;
+import com.atn.anhtuankg1997.appnhac.Service.APIService;
+import com.atn.anhtuankg1997.appnhac.Service.Dataservice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class Fragment_Banner extends Fragment {
@@ -21,6 +30,26 @@ public class Fragment_Banner extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_banner, container,false);
+        GetData();
         return view;
     }
+
+    private void GetData() {
+        Dataservice dataservice = APIService.getService();
+        Call<List<Quangcao>> callback = dataservice.GetDataBanner();
+        callback.enqueue(new Callback<List<Quangcao>>() {
+            @Override
+            public void onResponse(Call<List<Quangcao>> call, Response<List<Quangcao>> response) {
+                ArrayList<Quangcao> banner = (ArrayList<Quangcao>) response.body();
+                Log.d("AAA", banner.get(0).getTenBaiHat());
+            }
+
+            @Override
+            public void onFailure(Call<List<Quangcao>> call, Throwable t) {
+
+            }
+        });
+    }
+
+
 }
--- SourceCode/app/src/main/java/com/atn/anhtuankg1997/appnhac/Service/Dataservice.java
+++ SourceCode/app/src/main/java/com/atn/anhtuankg1997/appnhac/Service/Dataservice.java
@@ -1,4 +1,15 @@
 package com.atn.anhtuankg1997.appnhac.Service;
 
+import com.atn.anhtuankg1997.appnhac.Model.Quangcao;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Dataservice {
+
+    @GET("songbanner.php")
+    Call<List<Quangcao>> GetDataBanner();
+
 }
